3f62ff636a935c0e0bf524aca7af857987c11f25
magmarleisegang <magmarleisegang@gmail.com>
2017-05-28 14:28:56 +0200

    Add file missing from commit #master a67f015

---
+++ BridgeInvoicing/ViewModels/SessionListGroup.cs
@@ -0,0 +1,25 @@
+﻿using BridgeInvoicing.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeInvoicing.ViewModels
+{
+    public class SessionListGroup : List<Session>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; } //will be used for jump lists
+        public string Subtitle { get; set; }
+        public SessionListGroup(IGrouping<int,Session> sessions)
+        {
+            Title = sessions.Key.ToString();
+            ShortName = sessions.Key.ToString();
+
+            var list = sessions.ToList();
+            list.Sort(new SessionDateComparer());
+            this.AddRange(list);
+        }
+    }
+}
